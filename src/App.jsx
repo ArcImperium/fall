ec66e889f62c402017ca0af3a09c07ddc8684b49
Eli Peters <elip271828@gmail.com>
2025-10-22 21:57:30 -0400

    Now with randomized position it's kind of buggy with updating code. the y-position where it changes x will vary depending on when code is updated (could result in future bugs)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import {useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
+  const [fallerX, setFallerX] = useState(null)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX(Math.random() * 100)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  })
+
   return (
     <>
-    <div className="faller"></div>
+    <div className="faller" style={{left: `${fallerX}%`}}></div>
     </>
   )
 }
