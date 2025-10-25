b3780fe4609443a64342b83554b6657d27f0d602
Eli Peters <elip271828@gmail.com>
2025-10-24 22:06:48 -0400

    Added more objects. Objects are now leaves. Leaves can be clicked. Score made.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,122 @@
 import {useState, useEffect} from 'react'
 import './App.css'
+import Leaf from './assets/leaf.png'
 
 function App() {
-  const [fallerX, setFallerX] = useState(null)
+  const [fallerX1, setFallerX1] = useState(10)
+  const [showFaller1, setShowFaller1] = useState(true)
+  const [fallerX2, setFallerX2] = useState(20)
+  const [showFaller2, setShowFaller2] = useState(true)
+  const [fallerX3, setFallerX3] = useState(30)
+  const [showFaller3, setShowFaller3] = useState(true)
+  const [fallerX4, setFallerX4] = useState(40)
+  const [showFaller4, setShowFaller4] = useState(true)
+  const [fallerX5, setFallerX5] = useState(50)
+  const [showFaller5, setShowFaller5] = useState(true)
+  const [fallerX6, setFallerX6] = useState(60)
+  const [showFaller6, setShowFaller6] = useState(true)
+  const [fallerX7, setFallerX7] = useState(70)
+  const [showFaller7, setShowFaller7] = useState(true)
+  const [fallerX8, setFallerX8] = useState(80)
+  const [showFaller8, setShowFaller8] = useState(true)
+  const [fallerX9, setFallerX9] = useState(90)
+  const [showFaller9, setShowFaller9] = useState(true)
+
+  const [score, setScore] = useState(0)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setFallerX(Math.random() * 100)
+      setFallerX1(Math.random() * 95)
+      setShowFaller1(true)
     }, 10000)
 
     return () => clearInterval(interval)
-  })
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX2(Math.random() * 95)
+      setShowFaller2(true)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX3(Math.random() * 95)
+      setShowFaller3(true)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX4(Math.random() * 95)
+      setShowFaller4(true)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX5(Math.random() * 95)
+      setShowFaller5(true)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX6(Math.random() * 95)
+      setShowFaller6(true)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX7(Math.random() * 95)
+      setShowFaller7(true)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX8(Math.random() * 95)
+      setShowFaller8(true)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFallerX9(Math.random() * 95)
+      setShowFaller9(true)
+    }, 10000)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <>
-    <div className="faller" style={{left: `${fallerX}%`}}></div>
+    <h1 className="score">Score: {score}</h1>
+    {showFaller1 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller1(false), setScore(prev => prev + 1)}} style={{left: `${fallerX1}%`}}/>)}
+    {showFaller2 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller2(false), setScore(prev => prev + 1)}} style={{left: `${fallerX2}%`}}/>)}
+    {showFaller3 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller3(false), setScore(prev => prev + 1)}} style={{left: `${fallerX3}%`}}/>)}
+    {showFaller4 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller4(false), setScore(prev => prev + 1)}} style={{left: `${fallerX4}%`}}/>)}
+    {showFaller5 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller5(false), setScore(prev => prev + 1)}} style={{left: `${fallerX5}%`}}/>)}
+    {showFaller6 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller6(false), setScore(prev => prev + 1)}} style={{left: `${fallerX6}%`}}/>)}
+    {showFaller7 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller7(false), setScore(prev => prev + 1)}} style={{left: `${fallerX7}%`}}/>)}
+    {showFaller8 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller8(false), setScore(prev => prev + 1)}} style={{left: `${fallerX8}%`}}/>)}
+    {showFaller9 && (<img src={Leaf} className="faller" onClick={() => {setShowFaller9(false), setScore(prev => prev + 1)}} style={{left: `${fallerX9}%`}}/>)}
     </>
   )
 }
